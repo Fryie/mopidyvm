eb3286d5c7b7f47194c9d5514e7103ce7a562627
Pierpaolo Frasa <pierpaolo.frasa@twt.de>
2014-01-28 10:03:36 +0100

    checkout mopidy repository

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,3 +1,14 @@
+package { 'git':
+  ensure => installed
+}
+->
+vcsrepo { '/vagrant/mopidy':
+  ensure   => latest,
+  provider => git,
+  source   => "git://github.com/Fryie/mopidy"
+}
+->
 class { 'python':
   pip => true
 }
+
