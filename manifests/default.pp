e4a64ea9d8424d44cb3dafa9f8b876071eb94c20
Pierpaolo Frasa <pierpaolo.frasa@twt.de>
2014-01-28 14:21:56 +0100

    install mopidy and dependencies

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,14 +1,33 @@
+package { 'openssl-devel':
+  ensure => installed
+}
+->
 package { 'git':
   ensure => installed
 }
 ->
-vcsrepo { '/vagrant/mopidy':
+vcsrepo { '/home/vagrant/mopidy':
   ensure   => latest,
   provider => git,
-  source   => "git://github.com/Fryie/mopidy"
+  source   => "git://github.com/Fryie/mopidy",
+  user     => 'vagrant'
 }
 ->
 class { 'python':
-  pip => true
+  pip        => true,
+  dev        => true,
+  virtualenv => true
 }
-
+->
+python::virtualenv { '/home/vagrant/env':
+  ensure     => present,
+  owner      => 'vagrant',
+  systempkgs => true,
+  timeout    => 0
+}
+->
+exec { '/home/vagrant/env/bin/python setup.py develop':
+  cwd         => '/home/vagrant/mopidy',
+  environment => "VIRTUAL_ENV=/home/vagrant/env",
+  user        => 'vagrant'
+}
